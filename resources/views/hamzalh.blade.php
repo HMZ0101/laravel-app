3186b198f35041c955262271a5d5e4b7a618c4fd
HAMZA <LH>
2023-05-03 10:55:21 +0100

    test

---
+++ resources/views/hamzalh.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>HAMZA</h2>
+    <p> Students & Writers mostly look for some questions when coming to paragraph writing about any topic or thing or person. The questions raised by most of the students while thinking about writing a paragraph are Paragraph Writing Examples, What is the perfect paragraph format? How many steps involved to write a paragraph? How to write a good paragraph? How many sentences are included in a para? and many more like these.</p>
+</body>
+</html>
